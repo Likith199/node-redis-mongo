ce85871a975e39eda24fa2ed1417a7e9ba8fe9ed
durga7 <103110808+Likith199@users.noreply.github.com>
2022-11-14 16:04:25 +0530

    Update cache.js

--- services/cache.js
+++ services/cache.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const redis = require('redis')
 const util = require('util')
 
-const redisUrl = 'redis://65.0.94.153:6379';
+const redisUrl = 'redis://54.176.209.183:6379';
 const client = redis.createClient(redisUrl);
 client.hget = util.promisify(client.hget);                // client get does not support promises. this is a way to promisify them
 
